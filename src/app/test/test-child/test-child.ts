bf93dd8db72a2192d4b241bf08f90a39e8b66028
fparker4th <fparker@deliveryman.com>
2026-07-15 12:29:59 -0400

    ADD IN

--- src/app/test/test-child/test-child.ts
+++ src/app/test/test-child/test-child.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-test-child',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './test-child.html',
   styleUrl: './test-child.scss',
 })
-export class TestChild {}
+export class TestChild {
+  @Input({ required: true }) title: string = '';
+}

f47240f97cf75c4e1195a82b824c71cde020ea94
fparker4th <fparker@deliveryman.com>
2026-07-16 17:22:57 -0400

    ome at a tiume

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Test } from './test/test';
+import { Home } from './home/home';
 
 export const routes: Routes = [
     { path: "test", component: Test },
+    { path: "home", component: Home },
     { path: "", redirectTo: "/test", pathMatch: "full" }
 ];

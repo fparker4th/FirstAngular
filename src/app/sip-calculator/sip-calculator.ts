b0a8e2cbbed2ea8162bf3ff7ca4bdcfb796e36b1
fparker4th <fparker@deliveryman.com>
2026-07-16 18:21:29 -0400

    add

---
+++ src/app/sip-calculator/sip-calculator.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sip-calculator',
+  imports: [],
+  templateUrl: './sip-calculator.html',
+  styleUrl: './sip-calculator.scss',
+})
+export class SipCalculator {}

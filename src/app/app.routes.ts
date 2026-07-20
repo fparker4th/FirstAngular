c2c81d8368bbb2ff6055ea8671851071b13f9851
fparker4th <fparker@deliveryman.com>
2026-07-20 19:41:01 -0400

    new com

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Home } from './home/home';
 import { Contact } from './contact/contact';
 import { About } from './about/about';
 import { SipCalculator } from './sip-calculator/sip-calculator';
+import { DashBoard } from './dash-board/dash-board';
 export const routes: Routes = [
     { path: "test", component: Test },
     { path: "contact", component: Contact },
     { path: "about", component: About },
     { path: "home", component: Home },
     { path: "sip-calculator", component: SipCalculator },
+    { path: "dash-board", component: DashBoard },
     { path: "", redirectTo: "/home", pathMatch: "full" }
 ];
---
+++ src/app/dash-board/dash-board.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dash-board',
+  imports: [],
+  templateUrl: './dash-board.html',
+  styleUrl: './dash-board.scss',
+})
+export class DashBoard {}

049690ed202a12e94b204c25483441761a1f3be5
fparker4th <fparker@deliveryman.com>
2026-07-20 22:32:45 -0400

    saave

--- src/app/dash-board/dash-board.ts
+++ src/app/dash-board/dash-board.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Employee } from '../model/employeeModel';
 
 @Component({
   selector: 'app-dash-board',
@@ -6,4 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './dash-board.html',
   styleUrl: './dash-board.scss',
 })
-export class DashBoard {}
+export class DashBoard {
+  salesValue = signal<number>(75000);
+  targetValue = signal<number>(1000);
+  isActive = signal<boolean>(false);
+  performance = signal<string>('average'); //options: poor, average, excellent
+  statusColor = signal<string>('green');
+
+  //Employee data for list demonstrations
+  employees = signal<Employee[]>(
+    [
+      { id: 1, name: 'John Smith', role: 'Manager', salary: 85000 },
+      { id: 2, name: 'Sarah Johnson', role: 'Developer', salary: 75000 },
+      { id: 3, name: 'Mike Davis', role: 'Designer', salary: 65000 }
+    ]);
+}
